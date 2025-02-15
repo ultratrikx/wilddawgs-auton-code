771b3f1107fc71e30507cf5015cece2404469eb0
ultratrikx <rohanthm@outlook.com>
2025-02-15 04:29:42 -0500

    Refine joystick input curves, update PID controller settings, and adjust autonomous behavior for improved navigation

--- src/main.cpp
+++ src/main.cpp
@@ -24,40 +24,65 @@ pros::ADIDigitalOut doinker('G');
 pros::ADIEncoder arm_encoder('A', 'B', false);
 
 // input curve for throttle input during driver control
-lemlib::ExpoDriveCurve throttle_curve(2, // joystick deadband out of 127
+lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
                                      10, // minimum output where drivetrain will move out of 127
                                      1.019 // expo curve gain
 );
 
 // input curve for steer input during driver control
-lemlib::ExpoDriveCurve steer_curve(2, // joystick deadband out of 127
+lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
                                   10, // minimum output where drivetrain will move out of 127
-                                  1.021 // expo curve gain
+                                  1.019 // expo curve gain
 );
 
-// lateral PID controller
+// // lateral PID controller
+// lemlib::ControllerSettings lateral_controller(0, // proportional gain (kP)
+//                                               0, // integral gain (kI)
+//                                               0, // derivative gain (kD)
+//                                               0, // anti windup
+//                                               0, // small error range, in inches
+//                                               0, // small error range timeout, in milliseconds
+//                                               0, // large error range, in inches
+//                                               0, // large error range timeout, in milliseconds
+//                                               0 // maximum acceleration (slew)
+// );
+
+// // angular PID controller
+// lemlib::ControllerSettings angular_controller(21, // proportional gain (kP)
+//                                               0, // integral gain (kI)
+//                                               155,// derivative gain (kD)
+//                                               0, // anti windup
+//                                               0, // small error range, in degrees
+//                                               0, // small error range timeout, in milliseconds
+//                                               0, // large error range, in degrees
+//                                               0, // large error range timeout, in milliseconds
+//                                               0 // maximum acceleration (slew)
+// );
+
+// lateral motion controller
 lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              25, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in inches
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in inches
-                                              0, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+                                            0, // integral gain (kI)
+                                            3, // derivative gain (kD)
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            20 // maximum acceleration (slew)
 );
 
-// angular PID controller
-lemlib::ControllerSettings angular_controller(21, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              155,// derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in degrees
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in degrees
-                                              0, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+// angular motion controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             10, // derivative gain (kD)
+                                             3, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             3, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
 );
+
 
 lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
 							nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
@@ -169,11 +194,12 @@ void competition_initialize() {}
 
 void autonomous()
 {
-	// set position to x:0, y:0, heading:0
-    chassis.setPose(0, 0, 0);
-    // turn to face heading 90 with a very long timeout
-    chassis.turnToHeading(90, 100000);
-
+	// Set the initial pose of the robot (x, y, heading)
+	chassis.setPose(-12, -12, 180); // Start position: 1 tile from right edge, facing backwards
+	// Move the robot backwards 44 inches
+	chassis.moveToPoint(-44, 0, 4000); // Move backwards to (-44, 0) maintaining 0 degree heading
+	clamp.set_value(false);
+	clamp.set_value(true);
 }
 
 
